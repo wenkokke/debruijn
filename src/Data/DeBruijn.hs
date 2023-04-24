832d0e4b5c4977717612a95d24f898a28cad30ff
Wen Kokke <wenkokke@users.noreply.github.com>
2023-04-24 14:58:27 +0100

    Fixes

--- src/Data/DeBruijn.hs
+++ src/Data/DeBruijn.hs
@@ -1,39 +1,111 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Data.DeBruijn
   ( DB (Z, S),
+    SomeDB (SomeDB, dbBound, dbIndex),
+    SomeSNat (SomeSNat),
+    SomePositiveSNat (SomePositiveSNat),
+    fromDB,
     raise,
     inject,
+    tabulate,
   )
 where
 
-import Data.Data (Proxy (..))
-import GHC.Num.Natural (Natural, naturalSubUnsafe)
-import GHC.TypeLits (natVal)
-import GHC.TypeNats (KnownNat, Nat, type (+))
+import GHC.Num.Natural (Natural, naturalIsZero, naturalSubUnsafe, naturalZero)
+import GHC.TypeNats (SNat, fromSNat, withSomeSNat, type (+))
+import Test.QuickCheck.Modifiers (Positive (..), NonNegative (..))
+import Test.QuickCheck.Arbitrary (Arbitrary (..))
+import Test.QuickCheck.Gen (Gen, elements)
 
-newtype DB (n :: Nat) = DB# Natural
+-- | The type of DeBruijn indices.
+newtype DB (n :: Natural) = UnsafeDB Natural
+  deriving (Eq, Ord)
 
+-- | Zero.
 pattern Z :: forall n. DB (1 + n)
-pattern Z = DB# 0
-
-pattern S :: forall n. DB n -> DB (1 + n)
-pattern S db <- (unsafeDBPred -> db)
+pattern Z <- (safePred -> Nothing)
   where
-    S (DB# index) = DB# (1 + index)
+    Z = UnsafeDB naturalZero
 
-unsafeDBPred :: DB (1 + n) -> DB n
-unsafeDBPred (DB# index) = DB# (index `naturalSubUnsafe` 1)
+-- | Successor.
+pattern S :: forall n. DB n -> DB (1 + n)
+pattern S db <- (safePred -> Just db)
+  where
+    S (UnsafeDB index) = UnsafeDB (1 + index)
 
-raise :: forall m n. (KnownNat m) => DB n -> DB (m + n)
-raise (DB# index) = let m = natVal (Proxy @m) in DB# (fromInteger m + index)
+-- | Take the precessor of the DeBruijn index.
+safePred :: DB (1 + n) -> Maybe (DB n)
+safePred (UnsafeDB index)
+  | naturalIsZero index = Nothing
+  | otherwise = Just (UnsafeDB (index `naturalSubUnsafe` 1))
 
-inject :: forall m n. (KnownNat m) => DB n -> DB (m + n)
-inject (DB# index) = DB# index
+{-# COMPLETE Z, S #-}
+
+-- | Convert a DeBruijn index to a Natural.
+fromDB :: DB n -> Natural
+fromDB (UnsafeDB index) = index
+
+instance Show (DB n) where
+  show :: DB n -> String
+  show (UnsafeDB index) = '#' : show index
+
+-- | An existential wrapper for DeBruijn indices.
+data SomeDB = forall n. SomeDB { dbBound :: SNat n, dbIndex :: DB n }
+
+instance Show SomeDB where
+  show :: SomeDB -> String
+  show (SomeDB _bound index) = show index
+
+-- | Raise the value of a DeBruijn index by some known natural `m`.
+raise :: SNat m -> DB n -> DB (m + n)
+raise m (UnsafeDB index) = UnsafeDB (fromSNat m + index)
+
+-- | Raise the range of a DeBruijn index by some known natural `m`.
+inject :: SNat m -> DB n -> DB (m + n)
+inject _m (UnsafeDB index) = UnsafeDB index
+
+-- | List all DeBruijn indices between `0` and some known natural `n`.
+tabulate :: SNat n -> [DB n]
+tabulate n = map UnsafeDB [0 .. fromSNat n]
+
+instance Arbitrary SomeDB where
+  arbitrary :: Gen SomeDB
+  arbitrary = do
+    SomePositiveSNat bound <- arbitrary
+    elements [SomeDB bound index | index <- tabulate bound]
+
+-- | This type represents unknown type-level natural number.
+data SomeSNat = forall n. SomeSNat (SNat n)
+
+deriving instance Show SomeSNat
+
+instance Arbitrary SomeSNat where
+  arbitrary :: Gen SomeSNat
+  arbitrary = do
+    n <- fromInteger . getNonNegative <$> arbitrary
+    return $ withSomeSNat n SomeSNat
+
+-- | This type represents unknown type-level positive number.
+data SomePositiveSNat = forall n. SomePositiveSNat (SNat n)
+
+deriving instance Show SomePositiveSNat
+
+instance Arbitrary SomePositiveSNat where
+  arbitrary :: Gen SomePositiveSNat
+  arbitrary = do
+    n <- fromInteger . getPositive <$> arbitrary
+    return $ withSomeSNat n SomePositiveSNat
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,16 @@
-module Main (main) where
+import Data.DeBruijn (SomeDB (SomeDB), SomeSNat (SomeSNat), fromDB, inject, raise)
+import GHC.TypeNats (fromSNat)
+import Test.QuickCheck (quickCheck)
+
+prop_injectIsIdentity :: SomeSNat -> SomeDB -> Bool
+prop_injectIsIdentity (SomeSNat m) (SomeDB _bound index) =
+  fromDB (inject m index) == fromDB index
+
+prop_raiseIsAdd :: SomeSNat -> SomeDB -> Bool
+prop_raiseIsAdd (SomeSNat m) (SomeDB _bound index) =
+  fromDB (raise m index) == fromSNat m + fromDB index
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  quickCheck prop_injectIsIdentity
+  quickCheck prop_raiseIsAdd
